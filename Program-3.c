0dbb1faee5269aadf29cff90c39769aa35d2d11b
mehakharbanda <122426669+mehakharbanda@users.noreply.github.com>
2023-03-09 10:54:37 +0530

    Update Program-3.c

---
+++ Program-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    const char i,n,product;
+    printf("Enter a number: ",n);
+    scanf("%c",&n);
+    product==1;
+    for (const char i=1;i<n;i==i+1) {
+        printf(product);
+    }
+    return 0;
+}
