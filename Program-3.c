6e98dcc6fa22d611e3a6ee783936ea3c44e2c913
mehakharbanda <122426669+mehakharbanda@users.noreply.github.com>
2023-03-09 10:53:13 +0530

    Update Program-3.c

---
+++ Program-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int a = 5, b = 7, c = 10;
+ 
+    if (a <= b && a <= c)
+        printf("%d is the smallest",a);
+ 
+    else if (b <= a && b <= c)
+        printf("%d is the smallest",b);
+ 
+    else
+        printf("%d is the smallest",c);
+ 
+    return 0;
+}
